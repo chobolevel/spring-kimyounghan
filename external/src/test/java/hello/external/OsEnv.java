5fabcfda792cfef95290adf34e84aec8d9f0a470
chobolevel <rodaka123@naver.com>
2023-03-23 23:24:22 +0900

    2023-03-23

--- external/src/test/java/hello/external/OsEnv.java
+++ external/src/test/java/hello/external/OsEnv.java
@@ -8,10 +8,14 @@ import java.util.Map;
 public class OsEnv {
 
     public static void main(String[] args) {
+
+
         Map<String, String> envMap = System.getenv();
         for (String key : envMap.keySet()) {
             log.info("env {}={}", key, System.getenv(key));
         }
+
+
         // 개발서버 DB_URL= dev.db.com
         // 운영서버 DB_URL = prod.db.com
         // System.getenv("DB_URL"); 로 호출해서 사용
