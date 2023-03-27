5746ef9adffc976efa4c3619aaa23f86a2d50f1e
chobolevel <rodaka123@naver.com>
2023-03-27 23:39:13 +0900

    2023-03-27

--- external-read/src/main/java/hello/datasource/MyDataSourcePropertiesV1.java
+++ external-read/src/main/java/hello/datasource/MyDataSourcePropertiesV1.java
@@ -2,6 +2,7 @@ package hello.datasource;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 import java.time.Duration;
 import java.util.ArrayList;
